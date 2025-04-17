59d04b4969f6e915a74efe64f785c1eee7ae81ad
robert <robert940121@gmail.com>
2025-04-17 22:53:47 +0800

    Fix: rotation matrix parameter and dt

--- src/obstacle_extractor.cpp
+++ src/obstacle_extractor.cpp
@@ -222,21 +222,21 @@ void ObstacleExtractor::localCallback(const nav_msgs::msg::Odometry& local_msg){
 }
 
 Point ObstacleExtractor::distortionCorrection(sensor_msgs::msg::LaserScan scan_msg, double* twist, double r, double phi){
-    double dt=scan_msg.time_increment;
+    double dt=scan_msg.scan_time;
     double c=1-abs((phi-scan_msg.angle_min)/(scan_msg.angle_max-scan_msg.angle_min));
 
     double d_theta=c*twist[2]*dt;
 
-    Eigen::Matrix3d R;
-    R << cos(d_theta), sin(d_theta), 0, -sin(d_theta), cos(d_theta), 0, 0, 0, 1;
+    Eigen::Matrix2d R;
+    R << cos(d_theta), -sin(d_theta), sin(d_theta), cos(d_theta);
 
-    Eigen::Vector3d curr2prev_in_curr_frame;
-    curr2prev_in_curr_frame << (-c*twist[0]*dt), (-c*twist[1]*dt), 0;
+    Eigen::Vector2d curr2prev_in_curr_frame;
+    curr2prev_in_curr_frame << (-c*twist[0]*dt), (-c*twist[1]*dt);
 
-    Eigen::Vector3d prev2scan_in_prev_frame;
-    prev2scan_in_prev_frame << (r*cos(phi)), r*sin(phi), 0;
+    Eigen::Vector2d prev2scan_in_prev_frame;
+    prev2scan_in_prev_frame << (r*cos(phi)), r*sin(phi);
     
-    Eigen::Vector3d curr2scan_in_curr_frame;
+    Eigen::Vector2d curr2scan_in_curr_frame;
     curr2scan_in_curr_frame = curr2prev_in_curr_frame + R*prev2scan_in_prev_frame;
     
     return Point(curr2scan_in_curr_frame(0), curr2scan_in_curr_frame(1));
